618c97f6174dfdb4b2e3f7f6bb13fc70ee9fb2ff
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2021-02-22 18:59:22 +0100

    добавить раздел "музей"

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const SELECTOR = {
 }
 
 const CLASS = {
-    hidden: 'hidden'
+    hidden: 'hidden',
+    tabpanelActive: 'museum__tabpanel__active',
+    tabActive: 'museum__tab__active'
 }
 
 const key = {
     ESC: 'Escape',
     TAB: 'Tab',
+    SPACE: 'Space',
+    ENTER: 'Enter',
+    UP: 'ArrowUp',
+    DOWN: 'ArrowDown',
+    LEFT: 'ArrowLeft',
+    RIGHT: 'ArrowRight',
+    HOME: 'Home',
+    END: 'End',
 }
 
 const openLoginModalButton = document.querySelector(SELECTOR.openLoginModalButton)
@@ -21,14 +31,26 @@ const overlay = document.querySelector(SELECTOR.overlay)
 const dialogWindow = document.querySelector(SELECTOR.loginModal)
 const submitLoginButton = document.querySelector(SELECTOR.submitLoginButton)
 
+const tabs = Array.from(document.querySelectorAll('.museum__tab'))
+const tabpanels = Array.from(document.querySelectorAll('.museum__tabpanel'))
+
 openLoginModalButton.addEventListener('click', () => {
     [overlay, dialogWindow].forEach(el => el.classList.remove(CLASS.hidden))
-    dialogWindow.focus()
+    closeLoginModalButton.focus()
 })
 closeLoginModalButton.addEventListener('click', () => {
     [overlay, dialogWindow].forEach(el => el.classList.add(CLASS.hidden))
     openLoginModalButton.focus()
 })
+
+tabs.forEach(tab => tab.addEventListener('click', (e) => {
+    e.preventDefault()
+    const controlledPanelId = e.target.getAttribute('aria-controls')
+    const panelToActivate = document.getElementById(controlledPanelId)
+
+    tabpanels.forEach(panel => panel.classList.remove(CLASS.tabpanelActive))
+    panelToActivate.classList.add(CLASS.tabpanelActive)
+}))
 
 window.addEventListener('keydown', (e) => {
     const { code, shiftKey } = e
@@ -48,7 +70,63 @@ window.addEventListener('keydown', (e) => {
                 e.preventDefault()
                 closeLoginModalButton.focus()
             }
-            break;
+            break
+        }
+        case key.LEFT: {
+            const tabIndex = tabs.indexOf(document.activeElement)
+            if (tabIndex !== -1) {
+                const nextIndex = tabIndex === 0 ? tabs.length - 1 : tabIndex - 1
+                activate(tabs, nextIndex, CLASS)
+            }
+            break
+        }
+        case key.RIGHT: {
+            const tabIndex = tabs.indexOf(document.activeElement)
+            if (tabIndex !== -1) {
+                const nextIndex = tabIndex === tabs.length - 1 ? 0 : tabIndex + 1
+                activate(tabs, nextIndex, CLASS)
+            }
+            break
+        }
+        case key.HOME: {
+            //TODO check the key name
+            const tabIndex = tabs.indexOf(document.activeElement)
+            if (tabIndex > 0) {
+                activate(tabs, 0, CLASS)
+            }
+            break
+        }
+        case key.END: {
+            //TODO check the key name
+            const tabIndex = tabs.indexOf(document.activeElement)
+            if (tabIndex >= 0) {
+                activate(tabs, tabs.length - 1, CLASS)
+            }
+            break
+        }
+        case key.SPACE:
+        case key.ENTER: {
+            const tabIndex = tabs.indexOf(document.activeElement)
+            if (tabIndex !== -1) {
+                const tab = tabs[tabIndex]
+                const controlledPanelId = tab.getAttribute('aria-controls')
+                const panelToActivate = document.getElementById(controlledPanelId)
+
+                tabs.forEach(tab => tab.classList.remove(CLASS.tabActive))
+                tabpanels.forEach(panel => panel.classList.remove(CLASS.tabpanelActive))
+
+                tab.classList.add(CLASS.tabActive)
+                panelToActivate.classList.add(CLASS.tabpanelActive)
+            }
+            break
         }
     }
 })
+
+function activate(items, index) {
+    items.forEach(item => {
+        item.setAttribute('tabindex', '-1')
+    })
+    items[index].setAttribute('tabindex', '0')
+    items[index].focus()
+}
